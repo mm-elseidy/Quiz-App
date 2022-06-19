64f8e716feaffe7838e4530f60241a7c218a3f10
Mahmoud Elseidy <105989542+mm-elseidy@users.noreply.github.com>
2022-06-19 22:49:03 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+// ============ User Exam Quiz ==============//
+
+var skip = document.getElementById("skip");
+var score = document.getElementById("score");
+var totalScore = document.getElementById("totalScore");
+var countdown = document.getElementById("countdown");
+var count = 0;
+var scoreCount = 0;
+var duartion = 0;
+var qaSet = document.querySelectorAll(".qa-set");
+var qaAnsRow = document.querySelectorAll(".qa-set .qa-ans-row input");
+
+skip.addEventListener("click", function() {
+    step();
+    duartion = 60;
+});
+
+qaAnsRow.forEach(function(qaAnsRowSingle) {
+    qaAnsRowSingle.addEventListener("click", function() {
+        setTimeout(function() {
+            step();
+            duartion = 60
+        }, 1000);
+
+        var valid = this.getAttribute("valid");
+        if (valid == "valid") {
+            scoreCount += 20;
+            score.innerHTML = scoreCount;
+            totalScore.innerHTML = scoreCount;
+        } else {
+            scoreCount -= 20;
+            score.innerHTML = scoreCount;
+            totalScore.innerHTML = scoreCount;
+        }
+    });
+});
+
+function step() {
+    count += 1;
+    for (var i = 0; i < qaSet.length; i++) {
+        qaSet[i].className = "qa-set";
+    }
+    qaSet[count].className = "qa-set active";
+    if (count == 5) {
+        skip.style.display = "none";
+        clearInterval(duartionTime);
+        countdown.innerHTML = 0;
+    }
+}
+
+var duartionTime = setInterval(function() {
+    if (duartion == 60) {
+        duartion = 0;
+    }
+    duartion += 1;
+    countdown.innerHTML = duartion;
+    if (countdown.innerHTML == "60") {
+        step()
+    }
+
+}, 1000)
